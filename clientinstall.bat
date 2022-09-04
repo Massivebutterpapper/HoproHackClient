ba3b4bfb217cdea5ab88030a3efec1e4613a9586
Massivebutterpapper <106040279+Massivebutterpapper@users.noreply.github.com>
2022-09-03 18:08:33 -0700

    cinstaller
    Installer it will be downloading stuff reopen the app for it to install again

---
+++ clientinstall.bat
@@ -0,0 +1,5 @@
+@echo off
+echo x=msgbox("In Construction" ,0, "Hopro Hacked Client For Lunar Client") >> hwellcom.vbs
+start hwellcom.vbs
+echo x=msgbox("Contact Player5695 or Massive [github/bandlab] If Bugs" ,0, "Report Bugs") >> rpb.vbs
+start rpb.vbs
